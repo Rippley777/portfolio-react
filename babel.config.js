2681fab8a0078e1d1b5c75d8d7e1f617ff7c49b8
Ally Rippley <ally.rippley@gmail.com>
2024-05-11 02:33:53 -0500

    remove stylex

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@stylexjs/babel-plugin',
-    ],
-    plugins: [
-      '@stylexjs/babel-plugin',
-      {
-        dev: process.env.NODE_ENV === 'development',
-        runtimeInjection: false,
-        genConditionalClasses: true,
-        treeshakeCompensation: true,
-        unstable_moduleResolution: {
-          type: 'commonJS',
-          rootDir: __dirname,
-        },
-      },
-    ],
+    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
   };
 };
